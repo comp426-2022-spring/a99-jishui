fcc46393879c3aaa4cc3b8767ded9805912d4a22
fdahnoun <fdahnoun@email.unc.edu>
2022-04-16 12:30:36 -0400

    added title

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import * as React from 'react';
 
 function App() {
   return (
     <div className="app">
         <h1>COVID-19 DASHBOARD</h1>
+        
     </div>
   );
 }
