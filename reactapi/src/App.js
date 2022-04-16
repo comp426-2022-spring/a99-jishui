ff96b7075ae48a06bcad719e0f2c17b2a3e9c841
stzheng7 <69952035+stzheng7@users.noreply.github.com>
2022-04-16 15:16:09 -0400

    Update App.js
    api fetch

--- reactapi/src/App.js
+++ reactapi/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://disease.sh/v3/covid-19/countries/usa')
+      .then(res=> res.json())
+      .then(json=> {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  } 
+
+  render() {
+    var{ isLoaded, items } = this.state;
+
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+    else {
+      return (
+        <div className="App">
+          <ul>
+            Country: {items.country} | Cases today: {items.todayCases}
+          </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
