0f431a9587e77c8f3ce79fd80a313cfc63fffa17
fdahnoun <fdahnoun@email.unc.edu>
2022-04-23 14:23:55 -0400

    added backend data numbers from api to frontend

--- src/App.js
+++ src/App.js
@@ -1,49 +1,77 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+// import React from 'react';
 import Data from './datainfo'
 import { FormControl, InputLabel, MenuItem, Select,} from "@mui/material"
 import Graph from "./graph"
-
-function App() {
-  return (
-    <><div className="app">
-      <div className="head">
-        <h1>COVID-19 DASHBOARD</h1>
-
-        
-        <FormControl className="dropdown_menu" fullWidth>
-          <InputLabel id="label_select">Select Option</InputLabel>
-          <Select variant="outlined">
-            <MenuItem value="current_cases">Current Cases</MenuItem>
-            <MenuItem value="deaths">Total Deaths</MenuItem>
-            <MenuItem value="vaccinated">Total Vaccinated</MenuItem>
-            <MenuItem value="hospitalized">Total Hospitalized</MenuItem>
+import React, { Component } from 'react';
 
 
-          </Select>
-        </FormControl>
-      </div>
+class App extends Component {
 
-      <div className="data_statistics">
-        <Data title="Coronavirus Cases" cases={123} total={2000}></Data>
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+    }
+  }
 
-        <Data title="Hospitalized" cases={123} total={3000}></Data>
+  componentDidMount() {
+    fetch('https://disease.sh/v3/covid-19/countries/usa')
+      .then(res=> res.json())
+      .then(json=> {
+        this.setState({
+          isLoaded: true,
+          items: json,
+        })
+      });
+  } 
 
-        <Data title="Vaccinated" cases={123} total={3000}></Data>
+  render() {
+    var{ isLoaded, items } = this.state;
 
-        <Data title="Deaths" cases={123} total={500}></Data>
-
-        
-      </div>
-
-    </div></>
-
-
-  );
- 
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    }
+    else {
+      return (
+        <><div className="app">
+          <div className="head">
+            <h1>COVID-19 DASHBOARD</h1>
+    
+            
+            <FormControl className="dropdown_menu" fullWidth>
+              <InputLabel id="label_select">Select Option</InputLabel>
+              <Select variant="outlined">
+                <MenuItem value="current_cases">Current Cases</MenuItem>
+                <MenuItem value="deaths">Total Deaths</MenuItem>
+                <MenuItem value="vaccinated">Total Vaccinated</MenuItem>
+                <MenuItem value="hospitalized">Total Hospitalized</MenuItem>
+    
+    
+              </Select>
+            </FormControl>
+          </div>
+    
+          <div className="data_statistics">
+            <Data title="Coronavirus Cases" cases={123} total={items.deaths}></Data>
+    
+            <Data title="Hospitalized" cases={123} total={3000}></Data>
+    
+            <Data title="Vaccinated" cases={123} total={3000}></Data>
+    
+            <Data title="Deaths" cases={123} total={500}></Data>
+    
+            
+          </div>
+    
+        </div></>
+    
+    
+      );
+    }
+  }
 }
-
-
 
 export default App;
