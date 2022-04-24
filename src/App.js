82b2f4016ec40cdf8fc2b3da4b750e0f4bfb83f2
SakDev <saketh.devareddy1@gmail.com>
2022-04-24 18:23:43 -0400

    Added login html. Changed css for the dropdown i think?

--- src/App.js
+++ src/App.js
@@ -1,77 +1,58 @@
 // import logo from './logo.svg';
 import './App.css';
-// import React from 'react';
+import React from 'react';
 import Data from './datainfo'
 import { FormControl, InputLabel, MenuItem, Select,} from "@mui/material"
 import Graph from "./graph"
-import React, { Component } from 'react';
+
+function App() {
+  return (
+    <><div className="app">
+      <h1>COVID-19 DASHBOARD</h1>
+    
+      <div className="head">
+
+      <div className="loginBox">
+        <a href="/login.html">Log In</a>
+        <a href="/signup.html">Sign Up</a>
+      </div>
+
+        <div className="box2">     
+          <FormControl className="dropdown_menu">
+            <InputLabel id="label_select">Select Option</InputLabel>
+            <Select variant="outlined">
+              <MenuItem value="current_cases">Current Cases</MenuItem>
+              <MenuItem value="deaths">Total Deaths</MenuItem>
+              <MenuItem value="vaccinated">Total Vaccinated</MenuItem>
+              <MenuItem value="hospitalized">Total Hospitalized</MenuItem>
 
 
-class App extends Component {
+            </Select>
+          </FormControl>
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      items: [],
-      isLoaded: false,
-    }
-  }
+          
+        </div>
+      </div>
 
-  componentDidMount() {
-    fetch('https://disease.sh/v3/covid-19/countries/usa')
-      .then(res=> res.json())
-      .then(json=> {
-        this.setState({
-          isLoaded: true,
-          items: json,
-        })
-      });
-  } 
+      <div className="data_statistics">
+        <Data title="Coronavirus Cases" cases={123} total={2000}></Data>
 
-  render() {
-    var{ isLoaded, items } = this.state;
+        <Data title="Hospitalized" cases={123} total={3000}></Data>
 
-    if (!isLoaded) {
-      return <div>Loading...</div>
-    }
-    else {
-      return (
-        <><div className="app">
-          <div className="head">
-            <h1>COVID-19 DASHBOARD</h1>
-    
-            
-            <FormControl className="dropdown_menu" fullWidth>
-              <InputLabel id="label_select">Select Option</InputLabel>
-              <Select variant="outlined">
-                <MenuItem value="current_cases">Current Cases</MenuItem>
-                <MenuItem value="deaths">Total Deaths</MenuItem>
-                <MenuItem value="vaccinated">Total Vaccinated</MenuItem>
-                <MenuItem value="hospitalized">Total Hospitalized</MenuItem>
-    
-    
-              </Select>
-            </FormControl>
-          </div>
-    
-          <div className="data_statistics">
-            <Data title="Coronavirus Cases" cases={items.cases} total={items.cases}></Data>
-    
-            <Data title="Recovered" cases={items.recovered} total={items.recovered}></Data>
-    
-            <Data title="Active Cases" cases={items.active} total={items.active}></Data>
-    
-            <Data title="Deaths" cases={items.deaths} total={items.deaths}></Data>
-    
-            
-          </div>
-    
-        </div></>
-    
-    
-      );
-    }
-  }
+        <Data title="Vaccinated" cases={123} total={3000}></Data>
+
+        <Data title="Deaths" cases={123} total={500}></Data>
+
+        
+      </div>
+
+    </div></>
+
+
+  );
+ 
 }
+
+
 
 export default App;
